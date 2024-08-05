b12ffd23ea0ebe0aa2fb86e937413b5189d5f2fd
divanvant <divanvant@gmail.com>
2024-08-05 16:16:37 +0200

    chore: setup repo

--- packages/web-components-stencil/src/button/stencil.tsx
+++ packages/web-components-stencil/src/button/stencil.tsx
@@ -6,15 +6,15 @@
 import { Component, h } from '@stencil/core';
 
 @Component({
-  tag: 'example-button',
+  tag: 'dronten-button',
   styleUrl: 'index.scss',
   shadow: true,
 })
 export class Button {
   render() {
     return (
-      <button class="example-button" type="button">
-        <span class="example-button__text">
+      <button class="dronten-button" type="button">
+        <span class="dronten-button__text">
           <slot></slot>
         </span>
       </button>
