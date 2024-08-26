cafe38f7de86a7465f43db77ce0a90680aa70e4e
divanvant <divanvant@gmail.com>
2024-08-26 13:55:31 +0200

    feat: add social icon button

---
+++ packages/components-react/src/Button/SocialIconButton.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { ForwardedRef, forwardRef } from 'react';
+import '@dronten/components-css/button/index.scss';
+
+type SocialIconButtonProps = {
+  link: string;
+  iconName: string;
+};
+
+export const SocialIconButton = forwardRef(
+  ({ link, iconName }: SocialIconButtonProps, ref: ForwardedRef<HTMLAnchorElement>) => {
+    return (
+      <a ref={ref} href={link} target="_blank" rel="noreferrer">
+        <i className={clsx('dronten-social-icon', `fab fa-${iconName}`)}></i>
+        <span className="sr-only">{iconName.split('-')[0]} van Gemeente Dronten, opent in nieuw tabblad</span>
+      </a>
+    );
+  },
+);
+
+SocialIconButton.displayName = 'SocialIconButton';
---
+++ packages/storybook/src/css-button-social-icon.stories.tsx
@@ -0,0 +1,61 @@
+/* @license CC0-1.0 */
+
+import readme from '@dronten/components-css/button/README.md?raw';
+import type { Meta, StoryObj } from '@storybook/react';
+import { PropsWithChildren } from 'react';
+import '@dronten/components-css/button/index.scss';
+
+const SocialIconButton = ({ link, iconName }: PropsWithChildren<{ link: string; iconName: string }>) => (
+  <div style={{ background: '#f8f8f8', padding: '1rem' }}>
+    <a href={link} target="_blank" rel="noreferrer">
+      <i className={`dronten-social-icon fab fa-${iconName}`}></i>
+      <span className="sr-only">{iconName.split('-')[0]} van Gemeente Dronten, opent in nieuw tabblad</span>
+    </a>
+  </div>
+);
+
+const meta = {
+  title: 'CSS Component/Buttons/SocialIconButton',
+  id: 'css-social-icon-button',
+  component: SocialIconButton,
+  argTypes: {
+    iconName: {
+      name: 'Content',
+      description: 'Button icon name (from fontawesome)',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+    link: {
+      name: 'Content',
+      description: 'Button Link',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    iconName: 'x-twitter',
+    link: '#',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof SocialIconButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Dronten social icon button',
+};
